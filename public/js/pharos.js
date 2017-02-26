8f9da461a2883970feb3512fb61638d27f1bd7eb
Natalie Euley <natalie.euley@gmail.com>
2017-02-25 22:57:50 -0600

    Merge remote-tracking branch 'origin/master'

--- public/js/pharos.js
+++ public/js/pharos.js
@@ -14,3 +14,89 @@ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     id: 'mapbox.mapbox-streets-v7',
     accessToken: 'pk.eyJ1IjoicmFuZG9tbHlrbmlnaHRlZCIsImEiOiJjaXpscXMwankwMmkyMzNud2lhcXRlNWcwIn0.UjT4dnxCymdv6d1AzVl70A'
 }).addTo(map);
+
+var curr_location = huntsville;
+
+if (navigator.geolocation) 
+{
+	navigator.geolocation.getCurrentPosition(getPosition, showError, {enableHighAccuracy: true, timeout: 1000, maximumAge: 0});
+} 
+else 
+{
+	alert('Geolocation is not supported in your browser');
+}
+
+function getPosition(position)
+{
+	var x = position.coords.latitude;
+	var y = position.coords.longitude;
+	curr_location = {lat: x, lng: y};
+}
+
+function showError(error)
+{
+	alert("Error: " + error);
+}
+
+function updateLocation()
+{
+	if (navigator.geolocation) 
+	{
+		navigator.geolocation.getCurrentPosition(getPosition, showError, {enableHighAccuracy: true, timeout: 1000, maximumAge: 0});
+	} 
+	else 
+	{
+		alert('Geolocation is not supported in your browser');
+	}
+}
+
+function dropPin(type)
+{
+	updateLocation();
+	switch(type)
+	{
+		case "vehicle":
+			var myIcon = L.icon({
+						iconUrl: 'img/Hackathon_PHAROS_LOGO_v1_pin-disabledvehicle-green.svg',
+						iconSize: [38, 95],
+						iconAnchor: [22, 94]
+					});
+			break;
+		case "flood":
+			var myIcon = L.icon({
+						iconUrl: 'img/Hackathon_PHAROS_LOGO_v1_pin-flooding-green.svg',
+						iconSize: [38, 95],
+						iconAnchor: [22, 94]
+					});
+			break;
+		case "person":
+			var myIcon = L.icon({
+						iconUrl: 'img/Hackathon_PHAROS_LOGO_v1_pin-personinneed-green.svg',
+						iconSize: [38, 95],
+						iconAnchor: [22, 94]
+					});
+			break;
+		case "fire":
+			var myIcon = L.icon({
+						iconUrl: 'img/Hackathon_PHAROS_LOGO_v1_pin-fire-green.svg',
+						iconSize: [38, 95],
+						iconAnchor: [22, 94]
+					});
+			break;
+		case "tree":
+			var myIcon = L.icon({
+						iconUrl: 'img/Hackathon_PHAROS_LOGO_v1_pin-fallentree-green.svg',
+						iconSize: [38, 95],
+						iconAnchor: [22, 94]
+					});
+			break;
+		case "road":
+			var myIcon = L.icon({
+						iconUrl: 'img/Hackathon_PHAROS_LOGO_v1_pin-roadclosure-green.svg',
+						iconSize: [38, 95],
+						iconAnchor: [22, 94]
+					});
+			break;
+	}
+	L.marker([curr_location.lat, curr_location.lng], {icon: myIcon}).addTo(map);
+}
